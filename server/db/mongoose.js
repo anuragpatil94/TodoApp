7b7282ecbf4741a0bb509d9e48f03bc6edad0c37
Anurag Patil <aspanurag@gmail.com>
2017-06-12 11:01:42 -0400

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+//if heroku ask for bill payment for mlabs addon.. go to mlab.com, create db, click on db, create user,then 
+//use command heroku config:set MONGODB_URI="<similar following string>"
+//mongoose.connect('mongodb://asd:asdasd@ds121622.mlab.com:21622/todoapp');
 
 module.exports = {
     mongoose
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var { User } = require('./models/user');
 var { Todo } = require('./models/todo');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -47,8 +48,8 @@ app.get('/todo/:id', (req, res) => {
     });
 })
 
-app.listen(3000, () => {
-    console.log('Started');
+app.listen(port, () => {
+    console.log(`server started at port ${port}`);
 });
 
 module.exports = { app };
